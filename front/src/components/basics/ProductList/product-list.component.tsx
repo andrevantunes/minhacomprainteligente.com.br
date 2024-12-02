41accece38aba803cbcd3057ae8dfa356901a3e3
Andre Antunes Vieira <andre.vieira@appmax.com.br>
2024-12-02 01:19:02 -0300

    feat: add filters

--- front/src/components/basics/ProductList/product-list.component.tsx
+++ front/src/components/basics/ProductList/product-list.component.tsx
@@ -1,18 +1,35 @@
 import type { ProductListProps } from "./product-list.types";
 
 import classNames from "classnames";
-import { TextField } from "@andrevantunes/andrevds";
+import { Label, TextField } from "@andrevantunes/andrevds";
 import { Product, Grid } from "@/components";
 import { useState } from "react";
 
 const ProductList = ({ children, className, propertyProducts, ...props }: ProductListProps) => {
   const cn = classNames("product-list", className);
   const [propertyProductsState, setPropertyProductsState] = useState(propertyProducts);
+  const categories = categoriesFromPropertyProducts(propertyProducts);
+  const [selectedCategory, setSelectedCategory] = useState("");
+  const [searchKeyState, setSearchKeyState] = useState("");
+
   const handleChangeFilter = (event: any) => {
     const searchKey = event.target.value.toLowerCase();
+    setSearchKeyState(searchKey);
     setPropertyProductsState(
       propertyProducts.filter((propertyProduct) =>
-        propertyProductFilter(propertyProduct, searchKey)
+        propertyProductFilter(propertyProduct, searchKey, selectedCategory)
+      )
+    );
+  };
+
+  const handleCategoryClick = (event: any) => {
+    const category = event.target.innerHTML;
+    if (selectedCategory === category) setSelectedCategory("");
+    else setSelectedCategory(category);
+
+    setPropertyProductsState(
+      propertyProducts.filter((propertyProduct) =>
+        propertyProductFilter(propertyProduct, searchKeyState, category)
       )
     );
   };
@@ -20,9 +37,21 @@ const ProductList = ({ children, className, propertyProducts, ...props }: Produc
     <div className={cn} {...props}>
       <div className="mb-2x">
         <TextField
+          id="product-filter"
           label="Pesquise pelo nome do produto ou categoria"
           onChange={handleChangeFilter}
         />
+      </div>
+      <div className="mb-2x flex gap-1x">
+        {categories.map((category) => (
+          <Label
+            onClick={handleCategoryClick}
+            key={category}
+            variant={selectedCategory === category ? "primary" : "secondary"}
+          >
+            {category}
+          </Label>
+        ))}
       </div>
       <Grid
         style={{ marginBottom: 60 }}
@@ -47,14 +76,22 @@ const ProductList = ({ children, className, propertyProducts, ...props }: Produc
   );
 };
 
-function propertyProductFilter(propertyProduct: any, searchKey: string) {
+function propertyProductFilter(propertyProduct: any, searchKey: string, category?: string) {
+  if (category && propertyProduct.category !== category) return false;
+
   if (searchKey.length < 2) return true;
   const propertySearcher =
     `${propertyProduct.category} ${propertyProduct.product.name}`.toLowerCase();
 
   const words = searchKey.split(" ").map((word: string) => word.trim());
 
-  return words.some((word: string) => propertySearcher.includes(word));
+  return words.every((word: string) => propertySearcher.includes(word));
+}
+
+function categoriesFromPropertyProducts(propertyProducts) {
+  const setter = new Set();
+  propertyProducts.forEach((propertyProduct) => setter.add(propertyProduct.category));
+  return [...setter.keys()];
 }
 
 export default ProductList;
