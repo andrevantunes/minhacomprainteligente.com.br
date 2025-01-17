93c4fe92a0e8294804ed4544d84cf0a3d6263ef5
Andre Antunes Vieira <andre.vieira@appmax.com.br>
2025-01-17 20:43:48 -0300

    feat: move one signal to head tag

--- front/src/pages/_document.tsx
+++ front/src/pages/_document.tsx
@@ -6,29 +6,29 @@ export default class CustomDocument extends Document {
     return (
       <Html lang="pt-BR">
         <Head>
-          <meta charSet="utf-8" />
-          <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
-          <link rel="icon" href="/favicon.ico" type="image/x-icon" />
-          <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-          <Script src="https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.page.js" defer />
-        </Head>
-        <body>
+          <meta charSet="utf-8"/>
+          <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon"/>
+          <link rel="icon" href="/favicon.ico" type="image/x-icon"/>
+          <meta httpEquiv="X-UA-Compatible" content="IE=edge"/>
+          <Script src="https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.page.js" defer/>
           <script
             dangerouslySetInnerHTML={{
               __html: `window.OneSignalDeferred = window.OneSignalDeferred || [];
-    OneSignalDeferred.push(async function (OneSignal) {
-      await OneSignal.init({
-        appId: process.env.NEXT_PUBLIC_ONESIGNAL_ID,
-        safari_web_id: process.env.NEXT_PUBLIC_ONESIGNAL_SAFARI_ID,
-        notifyButton: {
-          enable: true,
-        },
-      });
-    });`,
+OneSignalDeferred.push(async function (OneSignal) {
+  await OneSignal.init({
+    appId: process.env.NEXT_PUBLIC_ONESIGNAL_ID,
+    safari_web_id: process.env.NEXT_PUBLIC_ONESIGNAL_SAFARI_ID,
+    notifyButton: {
+      enable: true,
+    },
+  });
+});`,
             }}
           />
-          <Main />
-          <NextScript />
+        </Head>
+        <body>
+        <Main/>
+        <NextScript/>
         </body>
       </Html>
     );
