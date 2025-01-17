bce26598a9cb72a4b2229cb3c0195cffb0679187
Andre Antunes Vieira <andre.vieira@appmax.com.br>
2025-01-17 20:53:12 -0300

    feat: move one signal to head tag

--- front/src/pages/_document.tsx
+++ front/src/pages/_document.tsx
@@ -11,14 +11,14 @@ export default class CustomDocument extends Document {
           <link rel="icon" href="/favicon.ico" type="image/x-icon" />
           <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
           <Script src="https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.page.js" defer />
-          <Script
+          <script
             id="one-signal"
             dangerouslySetInnerHTML={{
               __html: `window.OneSignalDeferred = window.OneSignalDeferred || [];
 OneSignalDeferred.push(async function (OneSignal) {
   await OneSignal.init({
-    appId: process.env.NEXT_PUBLIC_ONESIGNAL_ID,
-    safari_web_id: process.env.NEXT_PUBLIC_ONESIGNAL_SAFARI_ID,
+    appId: ${process.env.NEXT_PUBLIC_ONESIGNAL_ID},
+    safari_web_id: ${process.env.NEXT_PUBLIC_ONESIGNAL_SAFARI_ID},
     notifyButton: {
       enable: true,
     },
